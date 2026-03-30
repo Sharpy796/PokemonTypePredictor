965115ec29f7eac9801ee7facb15689ea47eee52
Christian <christian.bankovic@gmail.com>
2026-03-30 12:01:40 -0500

    Removed data we don't need

--- module_tmp.py
+++ module_tmp.py
@@ -54,27 +54,27 @@ if __name__ == "__main__":
 #
 
 # Not sure if we need alll this, but I have this now
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.utils.data import Dataset
-from torch import nn
-from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
-from torchvision.transforms import ToTensor
-import scipy
-from scipy.stats import zscore
-from time import sleep
-from tqdm import tqdm
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, accuracy_score, recall_score, precision_score, f1_score, log_loss
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-from matplotlib.colors import LinearSegmentedColormap, ListedColormap
+# import torch
+# import torch.nn as nn
+# import torch.nn.functional as F
+# from torch.utils.data import Dataset
+# from torch import nn
+# from torch.utils.data import DataLoader
+# from torchvision import datasets, transforms
+# from torchvision.transforms import ToTensor
+# import scipy
+# from scipy.stats import zscore
+# from time import sleep
+# from tqdm import tqdm
+# from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, accuracy_score, recall_score, precision_score, f1_score, log_loss
+# import matplotlib.pyplot as plt
+# import matplotlib as mpl
+# from matplotlib.colors import LinearSegmentedColormap, ListedColormap
 import pandas as pd
 import numpy as np
-import kagglehub
-from kagglehub import KaggleDatasetAdapter
-import math
+# import kagglehub
+# from kagglehub import KaggleDatasetAdapter
+# import math
 
 #custom imports
 
@@ -94,14 +94,13 @@ import seaborn as sns
 
 
 #%% CONSTANTS                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
+CSV_FILEPATH = "pokemon_images/pokedex.csv"
 
 #%% CONFIGURATION               ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 
 #%% INITIALIZATIONS             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 
 #%% DECLARATIONS                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -118,9 +117,36 @@ def main():
     pass
 #
 
+def assign_spritepaths(df):
+    df_copy = df.copy()
+    df_filepaths = pd.DataFrame(columns=['spritepath_front','spritepath_back'])
+    root = "pokemon_images/sprites/"
+    new_row = {}
+    for i,entry in df_copy.iterrows():
+        new_row = {'spritepath_front': root+f"{entry['id']:04d}"+'-'+entry['name']+'-'+str(entry['pokedex_id'])+'/front/normal',
+                   'spritepath_back': root+f"{entry['id']:04d}"+'-'+entry['name']+'-'+str(entry['pokedex_id'])+'/back/normal',}
+        df_filepaths.loc[len(df_filepaths)] = new_row
+    #
+    df_copy = pd.concat([df_copy, df_filepaths], sort=False, axis=1)
+    return df_copy
+#
+
 #%% MAIN CODE                  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #Main code start here
-print("Hello world")
+df = pd.read_csv(CSV_FILEPATH)
+
+# Preprocessing
+
+# Remove entries we don't want to use 
+df = df[df['image_fn'] != '[]']
+df = df[df['gigantamax'] == False]
+df = df[df['mega_evolution'] == False]
+# Reset the index
+df.reset_index(inplace=True, drop=True)
+# Remove columns we aren't using
+df = df.drop(['shape','legendary','mega_evolution','alolan_form','galarian_form','gigantamax','image_fn'], axis=1)
+# Find the filepaths to each sprite we want to use
+df = assign_spritepaths(df)
 
 
 #%% SELF-RUN                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
